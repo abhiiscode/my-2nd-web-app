5f3d0808210594825b95238322bcf48204dee401
git_copilet <abktpass@gmail.com>
2024-06-17 13:55:49 +0530

    Day 22: add description, images and links

--- main.py
+++ main.py
@@ -1,15 +1,44 @@
-import streamlit as st
-st.set_page_config(layout="wide")
-#'''st = module, columns = method, col1,2 = object instences'''
+import streamlit as sl
+import pandas
 
-col1, col2 = st.columns(2)
+sl.set_page_config(layout="wide")
+#'''sl = module, columns = method, col1,2 = object instences'''
+
+col1, col2 = sl.columns(2)
 #'''image = method that produce image'''
 with col1:
-    st.image("images/photos.png")
+    sl.image("images/photos.png")
 
 with col2:
-    st.title("Abhishek Thorat")
+    sl.title("Abhishek Thorat")
     content = '''
     Hi I'm Abhishek! I'm python programmer, teacher and business consultant. I work as a senior business consultant in 2021. I work with various businessmen. 
     '''
-    st.info(content)  #st.write(content)
+    sl.info(content)  #sl.write(content)
+contact2 = '''
+Below you can find some of the apps of I have built in python.
+'''
+sl.info(contact2)
+
+empty_head, col_header, empty_head2 = sl.columns([1.2, 0.5, 1.2])
+with col_header:
+    col_header = sl.header("APP'S")
+    # good_header = sl.info(col_header)
+
+col3, empty_col, col4 = sl.columns([1.5, 0.5, 1.5])
+
+df = pandas.read_csv("data.csv", sep=";")
+
+with col3:
+    for index, row in df[:10].iterrows():
+        sl.header(row["title"])
+        sl.subheader(row["description"])
+        sl.image("images/" + row["image"])
+        sl.write(f"[source code]({row['url']})")
+
+    with col4:
+        for index, row in df[10:].iterrows():
+            sl.subheader(row["title"])
+            sl.subheader(row["description"])
+            sl.image("images/" + row["image"])
+            sl.write(f"[source code]({row['url']})")
