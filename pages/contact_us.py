c019a88af7e0f2f7ba266ad9eb47462ddf3f85f0
git_copilet <abktpass@gmail.com>
2024-06-20 12:49:21 +0530

    Day 24: final

--- pages/contact_us.py
+++ pages/contact_us.py
@@ -1,6 +1,8 @@
 import streamlit as sl
 from send_email import send_email_fung
 
+sl.set_page_config("wide")
+
 sl.header("For More Information CONTACT Us!")
 
 with sl.form(key="email_form"):
@@ -8,13 +10,15 @@ with sl.form(key="email_form"):
     user_message = sl.text_area("Enter your message")
 #1    info_message = info_message + "\n" + user_email
     #for f"" \ is very imp
-    info_message = f"""\       
-Subject: New Email from {user_email}
+    #1st
+    info_message = f"""\
+Subject: New Email from {user_email}           
+
 From: {user_email} 
 {user_message}
 """
-    '''it will send sender email address'''
+   # '''it will send sender email address'''
     button = sl.form_submit_button("Submit")
 if button:
-    send_email_fung(info_message)  #fist
+    send_email_fung(info_message)  #2nd
     sl.info("Your Email Was Send Sucessfully!")
--- send_email.py
+++ send_email.py
@@ -1,11 +1,14 @@
 import smtplib, ssl
+import os
 
-def send_email_fung(info_messgae):  #second
+def send_email_fung(info_messgae):  #3rd
+
     host = "smtp.gmail.com"
     port = 465
 
     username = "pythonwithabhi@gmail.com"
-    password = "aoyn uzec jjjyidld"  #add passwoed when ur code is completed
+    password = os.getenv("PASSWORD_for_app2")
+    #add passwoed when ur code is completed
 
     receiver = "abktpass@gmail.com"
     my_contaxt = ssl.create_default_context()
@@ -18,7 +21,7 @@ def send_email_fung(info_messgae):  #second
 
     with smtplib.SMTP_SSL(host, port, context=my_contaxt) as server:
         server.login(username, password)
-        server.sendmail(username, receiver, info_messgae) #3rd
+        server.sendmail(username, receiver, info_messgae) #4th
 
 
 
